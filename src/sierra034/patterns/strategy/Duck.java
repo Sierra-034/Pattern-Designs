35a876703641577a82cb5cb58100b72abc482e98
Samuel Gomez Balderas <sierra_034@outlook.com>
2018-01-22 16:49:53 -0600

    It started with a simple sumDuck app
    The initial designers of the system used standard OO techniques and
    created one Duck superclass from which all other duck types inherit.

---
+++ src/sierra034/patterns/strategy/Duck.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sierra034.patterns.strategy;
+
+/**
+ *
+ * @author Samuel Gomez
+ */
+public abstract class Duck {
+    
+    public void quack(){
+        System.out.println("Quacking¡");
+    }
+    
+    public void swim(){
+        System.out.println("Swimming");
+    }
+    
+    public abstract void display();
+}
---
+++ src/sierra034/patterns/strategy/MallardDuck.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sierra034.patterns.strategy;
+
+/**
+ *
+ * @author Samuel Gomez
+ */
+public class MallardDuck extends Duck {
+
+    @Override
+    public void display() {
+        System.out.println("Looks like a Mallard");
+    }    
+}
