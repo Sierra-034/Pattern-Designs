2219afb9370375213218779ab740961f01308e03
Samuel Gomez Balderas <sierra_034@outlook.com>
2018-01-28 01:39:36 -0600

    Implementing the weather station

---
+++ src/sierra034/patterns/observer/weatherStation/Observer.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sierra034.patterns.observer.weatherStation;
+
+/**
+ *
+ * @author Samuel Gomez
+ */
+public interface Observer {
+    
+    void update( float temp, float humidity, float pressure );
+}
---
+++ src/sierra034/patterns/observer/weatherStation/Subject.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sierra034.patterns.observer.weatherStation;
+
+/**
+ *
+ * @author Samuel Gomez
+ */
+public interface Subject {
+    
+    void registerObserver( Observer o );
+    void removeObserver( Observer o );
+    void notifyObservers();
+}
