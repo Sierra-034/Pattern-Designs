47d8fb4ccda68fcde8696b76a7dcbd3541129394
Samuel Gomez Balderas <sierra_034@outlook.com>
2018-01-31 03:00:08 -0600

    Strategy pattern definition neutral implementation

---
+++ src/sierra034/patterns/strategy/definition/ConcreteStrategyB.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sierra034.patterns.strategy.definition;
+
+/**
+ *
+ * @author Samuel Gomez
+ */
+public class ConcreteStrategyB implements Strategy {
+
+    @Override
+    public void operation() {
+        System.out.println("ConcreteStrategyB operation");
+    }
+    
+}
