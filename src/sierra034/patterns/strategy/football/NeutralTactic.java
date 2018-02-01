1a55fbc0b93f548576a3a78384fe157944744768
Samuel Gomez Balderas <sierra_034@outlook.com>
2018-01-31 22:46:52 -0600

    Strategy pattern football example

---
+++ src/sierra034/patterns/strategy/football/NeutralTactic.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sierra034.patterns.strategy.football;
+
+/**
+ *
+ * @author Samuel Gomez
+ */
+public class NeutralTactic implements FootballStrategy {
+
+    @Override
+    public void adhereTactic(String team) {
+        System.out.print(team + NEUTRAL_MESSAGE);
+    }
+    
+}
